0b35147c5869df9165adacfab2fe671c02bacf15
Simone <simone.biffi.91@gmail.com>
2018-01-04 17:53:13 +0100

    add gitignore

---
+++ __init__.py
@@ -0,0 +1,12 @@
+__author__ = 'Simone Biffi'
+
+from bs4 import BeautifulSoup
+import urllib
+
+'''
+    Atum is an Egyptian deity, he was the creator god, self-generated.
+    The aims of this project is to create dataset scraping the web
+'''
+
+if __name__ == "__main__":
+    pass
